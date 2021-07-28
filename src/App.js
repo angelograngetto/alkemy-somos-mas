7247fa2eba6c650b5e6c0a3adea6afc5e9510c6c
Nicolás Gomez <nicx_g@hotmail.com>
2021-07-28 02:31:55 -0300

    Backoffice component with welcome message added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import Backoffice from './Components/Backoffice/Index';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Redirect exact from="/" to="/home" />
+        <Route component={Backoffice} path="/backoffice" />
       </Switch>
     </Router>
   );
