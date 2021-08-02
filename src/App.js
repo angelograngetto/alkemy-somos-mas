404bcce16f64d0ba94f6bb2e8d3cdb14f5e0fc16
Nicolás Gomez <nicx_g@hotmail.com>
2021-08-02 20:00:59 +0000

    Merged in feature/OT54-31-formulario-edición---creación-de (pull request #11)
    Feature/OT54-31 formulario edición   creación de
    
    * axios added and basic structure form of slidesForm added
    
    * slide creation validation added
    
    * added edit functionality
    
    * design and details added
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-31-formulario-edición---creación-de
    
    * refactoring code with Yup
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-31-formulario-edición---creación-de
    
    * added an alert when the slide doesn't exist
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-31-formulario-edición---creación-de
    
    
    Approved-by: Pablo Gotelli

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import './App.css';
 
 import Backoffice from './Components/Backoffice/Index';
+import SlidesForm from './Components/Slides/SlidesForm';
 import Organization from './Components/Backoffice/Organization/Index';
 import RegisterForm from './Components/Auth/RegisterForm';
 import UsersForm from './Components/Users/UsersForm';
@@ -16,6 +17,7 @@ function App() {
     <Router>
       <Switch>
         <Redirect exact from="/" to="/home" />
+        <Route exact component={SlidesForm} path="/backoffice/slides/create" />
         <Route component={RegisterForm} path="/register" />
         <Route exact component={UsersForm} path="/backoffice/users/create" />
         <Route path="/home" />
--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,49 +1,258 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, { useEffect, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import CKEditor from 'ckeditor4-react';
+import {
+  Button,
+  Alert,
+  AlertIcon,
+  FormControl,
+  FormErrorMessage,
+  Input,
+  Box,
+  Container,
+  Link,
+} from '@chakra-ui/react';
+import * as Yup from 'yup';
 
-const SlidesForm = () => {
-  const [initialValues, setInitialValues] = useState({
-    name: '',
-    description: '',
+import axios from 'axios';
+
+const SlidesForm = ({ slide }) => {
+  const [slides, setSlides] = useState([]);
+  const [utils, setUtils] = useState({
+    error: null,
+    success: null,
+    loading: null,
+  });
+  useEffect(() => {
+    const getSlides = async () => {
+      try {
+        const response = await axios.get('http://ongapi.alkemy.org/api/slides');
+        return response.data.data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    };
+    getSlides()
+      .then((resp) => setSlides(resp))
+      .catch((err) => {
+        setSlides([]);
+        alert('No se pudo obtener los datos solicitados');
+      });
+  }, []);
+
+  const convertBase64 = (file) => {
+    if (file) {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = function () {
+          resolve(fileReader.result);
+        };
+
+        fileReader.onerror = function () {
+          reject(Error('Algo salió mal, vuelve a intentarlo'));
+        };
+      });
+    }
+  };
+
+  const formatFileSupported = ['image/jpg', 'image/png', 'image/jpeg'];
+  const slideOrders = slides
+    .filter((item) => item?.order !== slide?.order)
+    .map((item) => item?.order);
+  const validateSchema = Yup.object().shape({
+    slideName: Yup.string()
+      .required('Campo requerido')
+      .min(4, 'El nombre debe tener una longitud mínima de 4 carácteres'),
+    slideDescription: Yup.string().required('Campo requerido'),
+    slideOrder: Yup.mixed()
+      .required('Campo requerido')
+      .test({
+        name: 'orderNumberValidation',
+        exclusive: true,
+        message: 'Este campo tiene que ser numérico y mayor o igual a 0',
+        test: (value) => !isNaN(value) && value >= 0,
+      })
+      .test({
+        name: 'uniqueOrderValidation',
+        exclusive: true,
+        message: 'Este orden ya fue utilizado en otro Slide, prueba con otro',
+        test: (value) => !slideOrders.includes(parseInt(value)),
+      }),
+    file: Yup.mixed()
+      .required('Campo requerido')
+      .test({
+        name: 'formatFile',
+        exclusive: true,
+        message: 'El formato de imagen es inválido, sólo se permite .png o .jpg',
+        test: (value) => formatFileSupported.includes(value?.image?.type) || value.url,
+      }),
   });
 
-  const handleChange = (e) => {
-    if (e.target.name === 'name') {
-      setInitialValues({ ...initialValues, name: e.target.value });
-    }
-    if (e.target.name === 'description') {
-      setInitialValues({ ...initialValues, description: e.target.value });
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
-  };
-
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        name="name"
-        placeholder="Slide Title"
-        type="text"
-        value={initialValues.name}
-        onChange={handleChange}
-      ></input>
-      <input
-        className="input-field"
-        name="description"
-        placeholder="Write the description"
-        type="text"
-        value={initialValues.description}
-        onChange={handleChange}
-      ></input>
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-    </form>
+    <Formik
+      enableReinitialize={true}
+      initialValues={{
+        slideName: slide?.name ?? '',
+        slideDescription: slide?.description ?? '',
+        slideOrder: slide?.order ?? '',
+        file: { url: slide?.image } ?? '',
+      }}
+      validationSchema={validateSchema}
+      onSubmit={(values, actions) => {
+        setUtils({ error: null, success: null, loading: true });
+        if (slide && !slide.id) return;
+        if (!slide) {
+          axios
+            .post('http://ongapi.alkemy.org/api/slides', {
+              name: values.slideName,
+              description: values.slideDescription,
+              order: values.slideOrder,
+              image: values.file.imageParsed,
+            })
+            .then((resp) => {
+              setUtils({ error: false, success: true, loading: false });
+              actions.resetForm({
+                values: {
+                  slideName: '',
+                  slideDescription: '',
+                  slideOrder: '',
+                  file: '',
+                },
+              });
+            })
+            .catch((err) => {
+              setUtils({ error: true, sucess: false, loading: false });
+            });
+        } else {
+          axios
+            .put(`http://ongapi.alkemy.org/api/slides/${slide?.id}`, {
+              name: values.slideName,
+              description: values.slideDescription,
+              order: values.slideOrder,
+              image: values.file.imageParsed,
+            })
+            .then((resp) => {
+              setUtils({ error: false, success: true, loading: false });
+            })
+            .catch((err) => {
+              setUtils({ error: true, sucess: false, loading: false });
+            });
+        }
+      }}
+    >
+      {({ setFieldValue, values, errors, touched, setTouched }) => (
+        <Container>
+          <Form>
+            <Box color="gray.900" fontSize="3xl" fontWeight="bold">
+              {slide ? 'Edición de Slide' : 'Creación de Slide'}
+            </Box>
+            {slide && !slide.id ? (
+              <Alert my="2" status="error">
+                <AlertIcon />
+                <Link as={RouterLink} to="backoffice/slides/create">
+                  Este slide no existe, haz clic aquí para crearlo
+                </Link>
+              </Alert>
+            ) : null}
+            <Field name="slideName">
+              {({ field, form, meta }) => (
+                <FormControl isInvalid={form.errors.slideName && form.touched.slideName} my="2">
+                  <Input id="slideName" placeholder="Nombre del slide" type="text" {...field} />
+                  {meta.touched && meta.error && <FormErrorMessage>{meta.error}</FormErrorMessage>}
+                </FormControl>
+              )}
+            </Field>
+
+            <FormControl isInvalid={errors.slideDescription && touched.slideDescription} my="2">
+              <CKEditor
+                data={values.slideDescription}
+                id="slideDescription"
+                name="slideDescription"
+                onBlur={(evt) => {
+                  setTouched({ ...touched, slideDescription: true }, true);
+                  setFieldValue('slideDescription', evt.editor.getData());
+                }}
+                onChange={(evt) => {
+                  setTouched({ ...touched, slideDescription: true }, true);
+                  setFieldValue('slideDescription', evt.editor.getData());
+                }}
+              />
+              {touched.slideDescription && errors.slideDescription && (
+                <FormErrorMessage>{errors.slideDescription}</FormErrorMessage>
+              )}
+            </FormControl>
+
+            <Field name="slideOrder">
+              {({ field, form, meta }) => (
+                <FormControl isInvalid={form.errors.slideOrder && form.touched.slideOrder} my="2">
+                  <Input id="slideOrder" placeholder="Orden del slide" type="text" {...field} />
+                  {meta.touched && meta.error && <FormErrorMessage>{meta.error}</FormErrorMessage>}
+                </FormControl>
+              )}
+            </Field>
+
+            <FormControl isInvalid={errors.file && touched.file} my="2">
+              <input
+                id="file"
+                name="file"
+                type="file"
+                onBlur={(evt) => setTouched({ ...touched, slideDescription: true }, true)}
+                onChange={async (event) => {
+                  const fileImage = event.currentTarget.files[0];
+                  const imageParsed = await convertBase64(fileImage);
+                  setTouched({ ...touched, file: true }, true);
+                  setFieldValue('file', {
+                    image: fileImage,
+                    imageParsed,
+                  });
+                }}
+              />
+              {errors.file && (typeof values.file === 'object' || values.file.length === 0) && (
+                <FormErrorMessage>{errors.file}</FormErrorMessage>
+              )}
+            </FormControl>
+
+            {/* {show the image if is a valid image} */}
+            {values.file && !errors.file && touched.file ? (
+              <img
+                alt={slide && values.file.url ? values?.description : values?.file?.image?.name}
+                src={slide && values.file.url ? values?.file.url : values?.file?.imageParsed}
+                style={{ height: '200px', width: '100%', objectFit: 'cover' }}
+              />
+            ) : null}
+
+            {utils.loading ? (
+              <Button isLoading colorScheme="blue" loadingText="Enviando" my="2">
+                Enviar
+              </Button>
+            ) : (
+              <Button colorScheme="blue" disabled={slide && !slide.id} my="2" type="submit">
+                Enviar
+              </Button>
+            )}
+
+            {utils.success ? (
+              <Alert my="2" status="success">
+                <AlertIcon />
+                {slide
+                  ? 'El slide ha sido editado exitosamente '
+                  : 'El slide se ha creado exitosamente'}
+              </Alert>
+            ) : utils.error ? (
+              <Alert my="2" status="error">
+                <AlertIcon />
+                {slide
+                  ? 'Algo salió mal editando el slide, inténtalo de nuevo'
+                  : 'Algo salió mal creando el slide, inténtalo de nuevo'}
+              </Alert>
+            ) : null}
+          </Form>
+        </Container>
+      )}
+    </Formik>
   );
 };
-
 export default SlidesForm;
