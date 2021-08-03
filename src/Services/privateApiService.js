f56681869cab2e3b97614bed7d80d7700a494dea
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-03 22:22:17 +0000

    Merged in feature/OT54-56-crear-método-que-devuelva-el-enc (pull request #26)
    Feature/OT54-56 crear método que devuelva el enc
    
    * The function is created to obtain the token from the localStorage
    
    * Token existence verification
    
    * Return an object
    
    
    Approved-by: Pablo Gotelli

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,29 @@
 import axios from 'axios';
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 1, //Aqui va el ID del equipo!!
+  },
+};
+
+export const getToken = () => {
+  const headerAuthorization = '';
+  const token = localStorage.getItem('token') || '';
+
+  if (token !== '') {
+    headerAuthorization = 'Bearer ' + token;
+  }
+
+  return {
+    headerAuthorization: headerAuthorization,
+  };
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get('https://jsonplaceholder.typicode.com/users', config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
 export default Get;
