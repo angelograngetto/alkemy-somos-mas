540bb77223e2bab2dcf8b0a09a2f40cf5e5244bf
Nicolás Gomez <nicx_g@hotmail.com>
2021-08-06 18:13:24 +0000

    Merged in feature/OT54-77-extender-el-servicio-http-base-p (pull request #42)
    Feature/OT54-77 extender el servicio http base p
    
    * fixing the put function and removing unused stuff
    
    * refactoring activiesServices with private api services
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-77-extender-el-servicio-http-base-p
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-77-extender-el-servicio-http-base-p
    
    * Fixing all functions and removing unused stuff
    
    * added get and delete method
    
    
    Approved-by: Pablo Gotelli

--- src/Components/Activities/ActivitiesService.js
+++ src/Components/Activities/ActivitiesService.js
@@ -1,33 +1,23 @@
-import axios from 'axios';
-
-const activitiesURL = 'http://ongapi.alkemy.org/api/activities';
-
+import { Post, Put, Get, Delete } from '../../Services/privateApiService';
 class ActivitiesService {
   static async create(activity) {
-    const response = await axios.post(
-      activitiesURL,
-      { ...activity },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
+    const response = await Post('/activities', { ...activity });
+    return response;
   }
 
   static async update(activity) {
-    const response = await axios.put(
-      `${activitiesURL}/${activity.id}`,
-      { ...activity },
-      {
-        mode: 'no-cors',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
-      },
-    );
-    console.log(response);
+    const response = await Put('/activities', activity.id, { ...activity });
+    return response;
+  }
+
+  static async remove(id) {
+    const response = await Delete('/activities', id);
+    return response;
+  }
+
+  static async getActivities(id) {
+    const response = await Get('/activities', id ? id : null);
+    return response;
   }
 }
 
--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,12 +1,6 @@
 /* eslint-disable no-console */
 import axiosInstance from './axiosInstance';
 
-const config = {
-  headers: {
-    Group: 54, //Aqui va el ID del equipo!!
-  },
-};
-
 export const getToken = () => {
   let headerAuthorization = '';
   const token = localStorage.getItem('token') || '';
@@ -20,29 +14,38 @@ export const getToken = () => {
   };
 };
 
-export const Get = (path, id) => {
-  const token = getToken();
-  axiosInstance
-    .get(`${path}/${id ? id : ''}`, {
-      headers: { Authorization: token.headerAuthorization },
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+export const Get = async (path, id) => {
+  try {
+    const token = getToken();
+    const resp = await axiosInstance.get(`${path}/${id ? id : ''}`, {
+      headers: {
+        Authorization: token.headerAuthorization,
+      },
+    });
+    return resp;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-export const Put = (endpoint, id, obj) => {
-  const token = getToken();
-  axiosInstance
-    .put(`${endpoint}/${id}`, obj, {
-      headers: { Authorization: token.headerAuthorization },
-    })
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-};
-export const Delete = async (url, id) => {
-  const token = getToken();
+export const Put = async (endpoint, id, obj) => {
   try {
-    const response = await instance.delete(`${url}/${id}`, {
+    const token = getToken();
+    const resp = await axiosInstance.put(`${endpoint}/${id}`, obj, {
+      headers: {
+        Authorization: token.headerAuthorization,
+      },
+    });
+    return resp;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const Delete = async (url, id) => {
+  try {
+    const token = getToken();
+    const response = await axiosInstance.delete(`${url}/${id}`, {
       headers: {
         Authorization: token.headerAuthorization,
       },
@@ -53,14 +56,16 @@ export const Delete = async (url, id) => {
   }
 };
 
-export default Get;
 export const Post = async (url, body) => {
   try {
-    const token = await getToken();
-    await axiosInstance.post(`${url}`, body, {
-      headers: { Authorization: token.headerAuthorization },
+    const token = getToken();
+    const response = await axiosInstance.post(`${url}`, body, {
+      headers: {
+        Authorization: token.headerAuthorization,
+      },
     });
+    return response;
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 };
