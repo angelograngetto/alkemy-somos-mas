b03ab52281805dac56af6164c1d73f5e91a41d6b
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-13 11:07:22 -0300

    The getSlidesList function and its implementation in the state are created.

--- src/features/slides/slidesSlice.js
+++ src/features/slides/slidesSlice.js
@@ -7,3 +7,38 @@ const initialState = {
   loading: false,
   error: '',
 };
+
+export const getSlidesList = createAsyncThunk(
+  'slides/fetchList',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await SlidesServices.getAll();
+      return response.data.data;
+    } catch (error) {
+      return rejectWithValue([], error);
+    }
+  },
+);
+
+const slidesSlice = createSlice({
+  name: 'slides',
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [getSlidesList.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [getSlidesList.fulfilled]: (state, { payload }) => {
+      state.slidesList = payload;
+      state.loading = false;
+      state.error = '';
+    },
+    [getSlidesList.rejected]: (state, { payload, error }) => {
+      state.slidesList = payload;
+      state.error = error;
+      state.loading = false;
+    },
+  },
+});
+
+export default slidesSlice.reducer;
