476f240881f79383f48141c4d5a0c509c066c370
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-13 10:51:33 -0300

    Necessary imports are made for redux-thunk and the initialState is created

--- src/features/slides/slidesSlice.js
+++ src/features/slides/slidesSlice.js
@@ -1,1 +1,9 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+import SlidesServices from '../../Services/SlidesService';
+
+const initialState = {
+  slidesList: [],
+  loading: false,
+  error: '',
+};
