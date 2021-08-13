d70b2ab6a8ee06824223d174d78d37fa6ca47bcb
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-13 13:49:19 -0300

    The reducer is implemented in the store

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import activitiesReducer from '../features/activities/activitiesSlice';
 import membersReducer from '../features/members/membersSlice';
+import slidesSlice from '../features/slides/slidesSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     activities: activitiesReducer,
     members: membersReducer,
+    slides: slidesSlice,
   },
 });
