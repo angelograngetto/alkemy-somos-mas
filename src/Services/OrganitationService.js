84d56cbbcb92f4e1cf737f91a53483e16dcd1d12
Angelo Grangetto <angelograngetto@gmail.com>
2021-08-13 15:14:32 +0000

    Merged in feature/OT54-108-crear-variables-de-entorno (pull request #74)
    Feature/OT54-108 crear variables de entorno
    
    * added endpoint to .env and .env.example
    
    * changed path with enviroment variable
    
    * solved conflicts
    
    
    Approved-by: Pablo Gotelli

--- src/Services/OrganitationService.js
+++ src/Services/OrganitationService.js
@@ -1,13 +1,13 @@
 import { Get, Post } from './privateApiService';
-
+const path = process.env.REACT_APP_ORGANIZATION_ENDPOINT;
 class OrganizationService {
   static async get() {
-    const response = await Get('organization');
+    const response = await Get(path);
     return response.data;
   }
 
   static async edit(organization) {
-    const response = await Post('organization', organization);
+    const response = await Post(path, organization);
     return response.data;
   }
 }
