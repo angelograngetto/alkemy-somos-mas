c8fd9d9243ff99709dc37fbf746a5817899a14bf
federico illuminato <federicoilluminato@hotmail.com>
2021-08-16 14:22:19 +0000

    Merged in feature/OT54-104-crear-variables-de-entorno (pull request #83)
    added enviorment variables
    
    * added enviorment variables
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-104-crear-variables-de-entorno
    
    
    Approved-by: Pablo Gotelli

--- src/Services/MembersService.js
+++ src/Services/MembersService.js
@@ -1,28 +1,28 @@
 import { Delete, Get, Post, Put } from './privateApiService';
-
+const path = process.env.REACT_APP_MEMBERS_ENDPOINT;
 class MembersService {
   static async getAll() {
-    const response = await Get('members');
+    const response = await Get(path);
     return response.data;
   }
 
   static async getById(id) {
-    const response = await Get('members', id);
+    const response = await Get(path, id);
     return response.data;
   }
 
   static async create(member) {
-    const response = await Post('members', member);
+    const response = await Post(path, member);
     return response.data;
   }
 
   static async update(member) {
-    const response = await Put('members', member.id, member);
+    const response = await Put(path, member.id, member);
     return response.data;
   }
 
   static async delete(id) {
-    const response = await Delete('members', id);
+    const response = await Delete(path, id);
     return response.data;
   }
 }
