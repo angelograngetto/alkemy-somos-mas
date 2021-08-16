dcf10b8c6a2a2c890192c40f1d80133eb4828fdc
Nicolás Gomez <nicx_g@hotmail.com>
2021-08-16 14:07:56 +0000

    Merged in feature/OT54-107-crear-variables-de-entorno (pull request #80)
    feature/OT54-107-crear-variables-de-entorno
    
    * environment variables added and services path modified with that variables
    
    
    Approved-by: Pablo Gotelli

--- src/Services/SlidesService.js
+++ src/Services/SlidesService.js
@@ -1,28 +1,28 @@
 import { Delete, Get, Post, Put } from './privateApiService';
-
+const path = process.env.REACT_APP_SLIDES_ENDPOINT;
 class SlidesService {
   static async getAll() {
-    const response = await Get('slides');
+    const response = await Get(path);
     return response.data;
   }
 
   static async getById(id) {
-    const response = await Get('slides', id);
+    const response = await Get(path, id);
     return response.data;
   }
 
   static async create(slide) {
-    const response = await Post('slides', slide);
+    const response = await Post(path, slide);
     return response.data;
   }
 
   static async update(slide) {
-    const response = await Put('slides', slide.id, slide);
+    const response = await Put(path, slide.id, slide);
     return response.data;
   }
 
   static async delete(id) {
-    const response = await Delete('slides', id);
+    const response = await Delete(path, id);
     return response.data;
   }
 }
