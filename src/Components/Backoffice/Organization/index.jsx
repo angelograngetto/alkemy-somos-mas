35a03108fe958c7eeb2bbf12243f7d1a2a879fdf
Angelo Grangetto <angelograngetto@gmail.com>
2021-08-29 19:33:28 +0000

    Merged in RodrigoCalderon_OrganizationComponent (pull request #132)
    RodrigoCalderon OrganizationComponent
    
    * Restyle component
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-171-filtrar-novedades-por-categoria
    
    * Mistakes solved
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into feature/OT54-171-filtrar-novedades-por-categoria
    
    * new mistake solved
    
    * Merge branch 'master'
    
    * Merge branch 'master' of https://bitbucket.org/alkemy-dev/t54-project into RodrigoCalderon_OrganizationComponent
    
    * little changes of style
    
    * restyled backoffice organization and edit organization component
    
    * restyled inputs
    
    
    Approved-by: Pablo Gotelli

--- src/Components/Backoffice/Organization/index.jsx
+++ src/Components/Backoffice/Organization/index.jsx
@@ -1,13 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Flex, Box, Container, Image, Button, Alert, AlertIcon } from '@chakra-ui/react';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
+import {
+  Flex,
+  Box,
+  Image,
+  Button,
+  Alert,
+  Divider,
+  AlertIcon,
+  Spacer,
+  Text,
+  VStack,
+  Stack,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Textarea,
+  FormLabel,
+} from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons';
 import OrganizationService from '../../../Services/OrganitationService';
 import ProgressBar from '../../Utils/ProgressBar';
+import {
+  FaFacebookF,
+  FaLinkedinIn,
+  FaTwitter,
+  FaInstagram,
+  FaPhoneAlt,
+  FaEnvelope,
+} from 'react-icons/fa';
 const Organization = () => {
   const history = useHistory();
   const [organization, setOrganization] = useState(null);
   const [utils, setUtils] = useState({ loading: false, error: false });
+  const [plainShortDescription, setShortPlainDescription] = useState('');
+  const [plainLongDescription, setLongPlainDescription] = useState('');
 
   const fetchOrganization = async () => {
     try {
@@ -24,65 +51,148 @@ const Organization = () => {
     fetchOrganization();
   }, []);
 
+  useEffect(() => {
+    const shortDescriptionHTML = organization?.short_description || '';
+    setShortPlainDescription(shortDescriptionHTML.replace(/<[^>]+>/g, '') || '');
+    const longDescriptionHTML = organization?.long_description || '';
+    setLongPlainDescription(longDescriptionHTML.replace(/<[^>]+>/g, '') || '');
+  }, [organization]);
+
   const toEdit = () => {
     history.push('/backoffice/organization/edit');
   };
 
-  const name = 'Name';
-  const image = 'https://via.placeholder.com/300x200?text=image';
-  const shortDescription = 'Lorem ipsum short description';
-
   return (
-    <Flex align="center" justify="center" minH="100vh">
-      <Container>
-        {utils.loading ? (
-          <Box m="-250px">
-            <ProgressBar colorScheme="blue" isIndeterminate={true} />
-          </Box>
-        ) : utils.error ? (
-          <Box alignItems="center" d="flex" justifyContent="center" minH="100vh">
-            <Alert
-              alignItems="center"
-              d="flex"
-              flexDirection="column"
-              justifyContent="center"
-              status="error"
-              textAlign="center"
-            >
-              <AlertIcon />
-              Hubo un error al cargar los datos, comprueba tu conexión a internet o intentálo de
-              nuevo
-              <Button
-                colorScheme="blue"
-                my="4"
-                variant="outline"
-                onClick={() => fetchOrganization()}
-              >
-                Volver a intentar
-              </Button>
-            </Alert>
-          </Box>
-        ) : (
-          <>
-            <Box borderRadius="lg" borderWidth="1px" maxW="lg" overflow="hidden">
-              <Image alt={name} src={image} />
-              <Box isTruncated as="h4" fontWeight="semibold" lineHeight="tight" m="2">
-                {name}
-              </Box>
-              <Box m="2">{shortDescription}</Box>
-            </Box>
-            <Button
-              colorScheme="teal"
-              margin="1rem"
-              rightIcon={<ArrowForwardIcon />}
-              variant="outline"
-              onClick={toEdit}
-            >
-              Edit
+    <Flex align="center" justify="center" minH="100vh" p={{ base: 0, sm: 0, lg: 5 }}>
+      {utils.loading ? (
+        <Box m="-250px">
+          <ProgressBar colorScheme="blue" isIndeterminate={true} />
+        </Box>
+      ) : utils.error ? (
+        <Box alignItems="center" d="flex" justifyContent="center" minH="100vh">
+          <Alert
+            alignItems="center"
+            d="flex"
+            flexDirection="column"
+            justifyContent="center"
+            status="error"
+            textAlign="center"
+          >
+            <AlertIcon />
+            Hubo un error al cargar los datos, comprueba tu conexión a internet o intentálo de nuevo
+            <Button colorScheme="blue" my="4" variant="outline" onClick={() => fetchOrganization()}>
+              Volver a intentar
             </Button>
-          </>
-        )}
-      </Container>
+          </Alert>
+        </Box>
+      ) : (
+        <Flex
+          borderRadius={{ base: '0', sm: '0', lg: 'xl' }}
+          borderWidth="1px"
+          boxShadow="2xl"
+          flexDir="column"
+          justify="center"
+          overflow="hidden"
+          p={{ base: 0, sm: 1, lg: 2 }}
+          w="5xl"
+        >
+          <Flex align="center" flexWrap="wrap" justify="space-between" m={{ base: 3, sm: 0 }}>
+            <Text
+              isTruncated
+              as="h1"
+              fontSize="xx-large"
+              fontWeight="semibold"
+              lineHeight="tall"
+              p="2"
+            >
+              Datos de la organización
+            </Text>
+            <Spacer />
+            <Button colorScheme="green" mr="2" title="Editar información" onClick={toEdit}>
+              Editar <EditIcon ml="2" />
+            </Button>
+          </Flex>
+          <Divider />
+
+          <Stack m="3" spacing={5}>
+            <Box>
+              <InputGroup boxShadow="lg" size="lg">
+                <InputLeftAddon>Nombre:</InputLeftAddon>
+                <Input readOnly boxShadow="lg" value={organization?.name} />
+              </InputGroup>
+            </Box>
+            <Image borderRadius="lg" boxShadow="lg" m="3" src={organization?.logo} />
+            <Box>
+              <FormLabel>Texto de bienvenida:</FormLabel>
+              <InputGroup boxShadow="lg" size="lg">
+                <Input readOnly boxShadow="lg" value={organization?.welcome_text} />
+              </InputGroup>
+            </Box>
+            <Box>
+              <FormLabel>Descripción corta:</FormLabel>
+              <InputGroup boxShadow="lg" size="lg">
+                <Textarea
+                  readOnly
+                  borderRadius="lg"
+                  boxShadow="lg"
+                  minH={{ sm: '10rem', md: '5rem' }}
+                  resize="none"
+                  value={plainShortDescription}
+                />
+              </InputGroup>
+            </Box>
+            <Box>
+              <FormLabel>Descripción larga:</FormLabel>
+              <InputGroup boxShadow="lg" size="lg">
+                <Textarea
+                  readOnly
+                  borderRadius="lg"
+                  boxShadow="lg"
+                  minH={{ sm: '10rem', md: '10rem' }}
+                  resize="none"
+                  value={plainLongDescription}
+                />
+              </InputGroup>
+            </Box>
+            <InputGroup boxShadow="lg" size="lg">
+              <InputLeftAddon color="blue.500">
+                <FaFacebookF />
+              </InputLeftAddon>
+              <Input readOnly value={organization?.facebook_url} />
+            </InputGroup>
+            <InputGroup boxShadow="lg" size="lg">
+              <InputLeftAddon color="blue.300">
+                <FaLinkedinIn />
+              </InputLeftAddon>
+              <Input readOnly value={organization?.linkedin_url} />
+            </InputGroup>
+            <InputGroup boxShadow="lg" size="lg">
+              <InputLeftAddon color="cyan.300">
+                <FaTwitter />
+              </InputLeftAddon>
+              <Input readOnly value={organization?.twitter_url} />
+            </InputGroup>
+            <InputGroup boxShadow="lg" size="lg">
+              <InputLeftAddon color="pink.300">
+                <FaInstagram />
+              </InputLeftAddon>
+              <Input readOnly value={organization?.instagram_url} />
+            </InputGroup>
+            <InputGroup boxShadow="lg" size="lg">
+              <InputLeftAddon color="green.300">
+                <FaPhoneAlt />
+              </InputLeftAddon>
+              <Input readOnly value={organization?.cellphone} />
+            </InputGroup>
+            <InputGroup boxShadow="lg" size="lg">
+              <InputLeftAddon color="red.300">
+                <FaEnvelope />
+              </InputLeftAddon>
+              <Input readOnly value="somosmas@ong.com" />
+            </InputGroup>
+          </Stack>
+        </Flex>
+      )}
     </Flex>
   );
 };
