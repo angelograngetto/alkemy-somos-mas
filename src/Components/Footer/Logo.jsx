12e5d516d50de621b40299251153f10d7f0570d0
Oscar Ariel Saucedo <arielsaucedo@gmail.com>
2021-08-29 22:46:27 -0300

    Footer image is modified

--- src/Components/Footer/Logo.jsx
+++ src/Components/Footer/Logo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Image, Stack } from '@chakra-ui/react';
-import LogoBg from '../../assets/LOGO-SOMOS-MAS.png';
+import LogoNav from '../../assets/LOGO-SOMOS-MAS-nav.png';
 
 export default function Logo({ url }) {
   return (
     <Stack alignItems="center" direction="column" justifyContent="center" w="100%">
-      {/* <Image borderRadius="50%" src={url} width={100} /> */}
-      <Image borderRadius="50%" src={LogoBg} width={150} />
+      <Image borderRadius="50%" src={LogoNav} width={150} />
     </Stack>
   );
 }
